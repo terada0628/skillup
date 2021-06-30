63cbe7f9e2f2d7062b4f0245010bd510ed1a361f
terada0628 <ntpeanuts@yahoo.co.jp>
2021-06-30 16:34:05 +0900

    start

---
+++ javascript/app.js
@@ -0,0 +1,20 @@
+var a = 1
+var b = 6
+var c = 7
+var d = 4
+var e = 6
+
+if(a > b){
+  alert('BさんよりAさんが高い');
+}else if(a > c){
+  alert('CさんよりAさんが高い');
+}else if(a > d){
+  alert('DさんよりAさんが高い');
+}else if(a > e){
+  alert('EさんよりAさんが高い');
+}else{
+  alert('Aさんは一番低い!!');
+}
+
+
+alert(a+b-c*d/e);

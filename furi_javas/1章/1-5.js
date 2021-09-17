66b9ca8fc3384521b392407f0663d60675d50518
terada0628 <ntpeanuts@yahoo.co.jp>
2021-09-17 13:12:20 +0900

    start1

---
+++ furi_javas/1章/1-5.js
@@ -0,0 +1,7 @@
+console.log(10+5);
+console.log(10-5);
+
+console.log(10*5);
+console.log(10/5);
+
+console.log(2+0.5);
---
+++ furi_javas/1章/1-6.js
@@ -0,0 +1,7 @@
+console.log(2+10+5);
+
+console.log(2+10*5);
+
+console.log((2+10)*5);
+
+console.log(5/(4*(1-0.2)));
